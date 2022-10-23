f5355be7740289dfb3c3c0c1d3dea9fb71e66dcb
GoldenFenixJM <0234666@up.edu.mx>
2022-10-22 19:39:19 -0500

    cambios

--- movies-app/src/hooks/useFetchTrending.js
+++ movies-app/src/hooks/useFetchTrending.js
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react"
 import { getTrending } from "../helpers/getTrending";
 
-export const useFetchTrending = () => {
+export const useFetchTrending = (SelectedOption, SelectedOption2) => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const getTrendingMovies = async (media_type, time_window) => {
-    const results = await getTrending(media_type, time_window);
+  const getTrendingMovies = async ( SelectedOption, SelectedOption2) => {
+    const results = await getTrending(SelectedOption, SelectedOption2);
     setMovies(results);
     setIsLoading(false);
   }
 
   useEffect(() => {
-    getTrendingMovies();
-  }, []);
+    getTrendingMovies(SelectedOption, SelectedOption2);
+  }, [SelectedOption, SelectedOption2]);
   
   return {
     movies,
--- movies-app/src/pages/Trending.js
+++ movies-app/src/pages/Trending.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import {useState} from 'react';
 import { MovieCard } from '../components/MovieCard';
 import { useFetchTrending } from '../hooks/useFetchTrending';
+import Select from 'react-select';
 
+
+
+const Opciones=[
+  {value: 'all', label: 'All'},
+  {value: 'movie', label: 'Movie'},
+  {value: 'tv', label: 'TV'},
+  {value: 'person', label: 'Person'},
+]
+const Opciones2=[
+  {value: 'day', label: 'Day'},
+  {value: 'week', label: 'Week'},
+]
 
 export const Trending = () => {
 
-  const {movies}= useFetchTrending();
-  
+  const {selectedOption, setSelectedOption}=useState(Opciones[0]);
+  const{ setSelectedOption2, selectedOption2}=useState (Opciones2[0]);
+  const {movies}= useFetchTrending(selectedOption, selectedOption2);
+  const handleChange=(value)=>{
+  console.log ('value', value);
+    const selectedOption=value.value
+    setSelectedOption(selectedOption);
+  }
+  const handleChange2=(value)=>{
+    console.log ('value', value);
+      const selectedOption2=value.value
+      setSelectedOption2(selectedOption2);
+    }
+
+
+
   
   return (
     <div className='container row row-cols-1 row-cols-md-3 g-3'>
+      <Select
+      options={Opciones}
+      onChange={handleChange}
+      />
+      <Select
+      options={Opciones2}
+      onChange={handleChange2}
+      />
        {movies.map((movie)=>
       <MovieCard 
         key={movie.id} 
